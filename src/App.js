2af58c3da364e1d3058ea546301f180e624cc97c
ng1n3 <code.with.muyiwa@gmail.com>
2024-02-06 18:10:08 +0100

    date counter functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-export default function App () {
+import { useState } from "react";
 
-  
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(0);
+  const date = new Date("feb 6 2024");
+  date.setDate(date.getDate() + count);
 
   return (
     <div>
+      <div>
+        <button onClick={() => setStep((currCount) => currCount - 1)}>
+          -
+        </button>
+        <span>step: {step}</span>
+        <button onClick={() => setStep((currCount) => currCount + 1)}>
+          +
+        </button>
+      </div>
+      <div>
+        <button onClick={() => setCount((currCount) => currCount - step)}>
+          -
+        </button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((currCount) => currCount + step)}>
+          +
+        </button>
+      </div>
 
+      <p>{date.toDateString()}</p>
     </div>
-  )
+  );
 }
-
